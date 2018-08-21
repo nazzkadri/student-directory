74e0345e4867bf28892272b506cbbf3fcf1e027c
EC2 Default User <ec2-user@ip-172-31-19-13.us-east-2.compute.internal>
2018-08-21 14:19:14 +0000

    practice center method and applied it

--- directory.rb
+++ directory.rb
@@ -5,41 +5,33 @@ def input_students
   students = []
   # get the first name
   name = gets.chomp
-  puts "Please enter your hobby"
-  hobby = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november, hobby: hobby}
-    puts "Now we have #{students.count} students"
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students".center(30)
     # get another name from the user
-    puts "Please enter your name"
     name = gets.chomp
-    puts "Please enter your hobby"
-    if name.empty? == true
-        break
-    end
-    hobby = gets.chomp
   end
   # return the array of students
   students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(60)
+  puts "-------------".center(60)
 end
 
 def print(students)
   i = 0
   while i < students.length do
-      puts "#{students[i][:name]} cohort #{students[i][:cohort]} hobby #{students[i][:hobby]}"
+      puts "#{students[i][:name]} cohort #{students[i][:cohort]}".center(60)
       i = i + 1
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(60)
 end
 
 students = input_students
