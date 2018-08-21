da65e04fd71a9c292ab8ff7a3d4d7a2777b06e1c
EC2 Default User <ec2-user@ip-172-31-19-13.us-east-2.compute.internal>
2018-08-21 13:57:19 +0000

    Hobbies added

--- directory.rb
+++ directory.rb
@@ -5,13 +5,21 @@ def input_students
   students = []
   # get the first name
   name = gets.chomp
+  puts "Please enter your hobby"
+  hobby = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, hobby: hobby}
     puts "Now we have #{students.count} students"
     # get another name from the user
+    puts "Please enter your name"
     name = gets.chomp
+    puts "Please enter your hobby"
+    if name.empty? == true
+        break
+    end
+    hobby = gets.chomp
   end
   # return the array of students
   students
@@ -25,7 +33,7 @@ end
 def print(students)
   i = 0
   while i < students.length do
-      puts "#{students[i][:name]} cohort #{students[i][:cohort]}"
+      puts "#{students[i][:name]} cohort #{students[i][:cohort]} hobby #{students[i][:hobby]}"
       i = i + 1
   end
 end
