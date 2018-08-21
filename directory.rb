0faa704dc05f5a315cb57fec79d68e9a03203ad0
EC2 Default User <ec2-user@ip-172-31-19-13.us-east-2.compute.internal>
2018-08-21 11:19:44 +0000

    added user-input

--- directory.rb
+++ directory.rb
@@ -1,35 +1,43 @@
 # Let's put all the students in an Array
-students = [
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}
-    ]
-
 def print_header
 puts "The students of Villains Academy"
 puts "------------------------"
 end
 
+def input_students
+  puts "Please enter the name of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name 
+    name = gets.chomp
+  end
+return students
+end
+
+
+
+
+# finally, we print the total number of students
 def print(students)
   students.each do |student|
-  puts "#{student[:name]} (#{student[:cohort]} cohort)"
+     puts "#{student[:name]} (#{student[:cohort]} cohort"
   end
 end
 
-# finally, we print the total number of students
 def print_footer(names)
 puts "Overall, we have #{names.count} great students"
 end
 
 #nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
