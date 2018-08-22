0a37838a330ba05df45ac6a85f42f6b47b10b869
EC2 Default User <ec2-user@ip-172-31-19-13.us-east-2.compute.internal>
2018-08-22 13:56:06 +0000

    exercise 10

--- directory.rb
+++ directory.rb
@@ -1,17 +1,18 @@
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  name = gets.delete!("\n")
   # while the name is not empty, repeat this code
   while !name.empty? do
   # add the student hash to the array
   students << {name: name, cohort: :november}
   puts "Now we have #{students.count} students"
   # get another name from the user
-  name = gets.chomp
+  name = gets.delete!("\n")
   end
   # return the array of students
   return students
@@ -24,7 +25,7 @@ end
 
 def print(students)
     students.each do |student|
-    puts ""#{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -35,8 +36,8 @@ end
 
 
 students = input_students
-if !students.empty? 
-     print_header
+if !students.empty?
+print_header
 print(students)
 print_footer(students)
 end
