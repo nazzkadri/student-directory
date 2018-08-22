30e69d16a531068e72118ed33a05fe6bab30b8ec
EC2 Default User <ec2-user@ip-172-31-19-13.us-east-2.compute.internal>
2018-08-22 14:27:29 +0000

    refactoring done

--- directory.rb
+++ directory.rb
@@ -1,42 +1,54 @@
+@students = [] # an empty array accessible to all methods
+
 def interactive_menu
-  students = []
   while true do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" 
-    selection = gets.chomp
-    case selection
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" 
+  return
+end
+
+def process(selection)
+  case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
       exit
     else
-      puts "I don't know what you meant, try again"
-    end
+      puts "I don't know what you mean, try again"
   end
+end
+
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
   # add the student hash to the array
-  students << {name: name, cohort: :november}
-  puts "Now we have #{students.count} students"
+  @students << {name: name, cohort: :november}
+  puts "Now we have #{@students.count} students"
   # get another name from the user
   name = gets.chomp
   end
   # return the array of students
-  return students
+  return @students
 end
 
 def print_header
@@ -44,16 +56,15 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-    students.each do |student|9
-    
+def print_students_list
+    @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
