2b822b2c64f0dabc21bcc3d9b01844a678eabf9f
EC2 Default User <ec2-user@ip-172-31-19-13.us-east-2.compute.internal>
2018-08-22 14:51:36 +0000

    student list saved to the file

--- directory.rb
+++ directory.rb
@@ -10,6 +10,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the student list to file students.csv"
   puts "9. Exit" 
   return
 end
@@ -20,6 +21,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when"3"
+      save_students  
     when "9"
       exit
     else
@@ -49,6 +52,16 @@ def input_students
   end
   # return the array of students
   return @students
+end
+
+def save_students
+  file = File.open("students.csv","w")
+  @students.each do |student|
+  student_data = [student[:name], student[:cohort]]
+  csv_line = student_data.join(",")
+  file.puts csv_line
+  end
+  file.close
 end
 
 def print_header
