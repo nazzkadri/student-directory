31992993ab4a713ccb86bb7b84b729bbd6613cea
EC2 Default User <ec2-user@ip-172-31-19-13.us-east-2.compute.internal>
2018-08-23 14:30:03 +0000

    quin file created reading its own source code and printing it

---
+++ quin.rb
@@ -0,0 +1,14 @@
+#print <<A * 2; print "A" + 10.chr
+#print <<A * 2; print "A" + 10.chr
+#A
+File.open($0, "r") do |f|
+puts f.read()
+end
+i = 0
+if i != 0
+    puts "true"
+else
+    puts "false"
+end
+
+
