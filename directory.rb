28bee4f3b5a526fc120d7d2312e1c6508234a9a5
EC2 Default User <ec2-user@ip-172-31-19-13.us-east-2.compute.internal>
2018-08-23 08:13:18 +0000

    loading from the file

--- directory.rb
+++ directory.rb
@@ -11,6 +11,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the student list to file students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" 
   return
 end
@@ -23,6 +24,8 @@ def process(selection)
       show_students
     when"3"
       save_students  
+    when "4"
+      load_students
     when "9"
       exit
     else
@@ -36,6 +39,16 @@ def show_students
   print_students_list
   print_footer
 end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+	
 
 def input_students
   puts "Please enter the names of the students"
